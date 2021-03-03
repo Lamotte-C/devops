86b79eacc902a8b2c1c2c026996d11514e9fc678
Cl�ment LAMOTTE <c.lamotte@iia-laval.fr>
2021-03-03 13:47:30 +0100

    1

--- DemoDevopsSolution/DemoDevopsProjet/Program.cs
+++ DemoDevopsSolution/DemoDevopsProjet/Program.cs
@@ -2,7 +2,7 @@
 
 namespace DemoDevopsProjet
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
---
+++ DemoDevopsSolution/DemoDevopsProjet/calculatrice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoDevopsProjet
+{
+    public class calculatrice
+    {
+        public calculatrice()
+        {
+
+        }
+
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int sub(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
+
+   

c270f4705630efcbfc0c9966aa24a38271a1c539
Cl�ment LAMOTTE <c.lamotte@iia-laval.fr>
2021-03-03 10:31:41 +0100

    Addproject

---
+++ DemoDevopsSolution/DemoDevopsProjet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoDevopsProjet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
